c93d3dd97d55f63994dc5be76b6075d42fda3700
Shantanu Jana <omni.7x@gmail.com>
2022-07-14 22:38:50 +0530

    updated app.js

--- My SuperHero App/app.js
+++ My SuperHero App/app.js
@@ -1,6 +1,7 @@
 const randomHeroBtn = document.querySelector('.random-hero-btn');
 const heroImg = document.querySelector('img');
 
+const baseURL = `https://superheroapi.com/api.php/160583143163979`; 
 
 const getRandomHeroId = () => {
     let randomHeroId = Math.floor(Math.random() * 731)+1;
@@ -9,29 +10,36 @@ const getRandomHeroId = () => {
 
 
 randomHeroBtn.addEventListener('click', () => {
-    fetch(`https://superheroapi.com/api.php/160583143163979/${getRandomHeroId()}`)
+    fetch(`${baseURL}/${getRandomHeroId()}`)
     .then(response => response.json())
     .then(json => {
         console.log(json.biography['full-name']);
-        // document.querySelector('body').innerHTML += `<img src="${json.image.url}" class="heroImg"/>`; 
         heroImg.setAttribute('src', `${json.image.url}`);
-        // console.log(json.image.url);
-
-
     });
 
     console.log(getRandomHeroId());
 
 });
 
-// const getSuperHero = () => {
-
-// };
 
 
 
 
+const searchBtn = document.querySelector('.search-btn');
+
+const getSearchedSuperHero = (name) => {
+    fetch(`${baseURL}/search/${name}`)
+    .then(response => response.json())
+    .then(json => {
+        const hero = json.results[0];
+        console.log(hero.name);
+        heroImg.setAttribute('src', `${hero.image.url}`);
+    });
+};
 
 
-
-
+searchBtn.addEventListener('click', () => {
+    let name = document.querySelector('#search-name').value;
+    
+    getSearchedSuperHero(name);
+})
