b85726487b4b74673501a5fd91924e7a0129dc2f
Shantanu Jana <omni.7x@gmail.com>
2022-12-08 21:54:36 +0530

    Completed Random Joke generator

--- Random_Joke_Generator/script.js
+++ Random_Joke_Generator/script.js
@@ -1,17 +1,18 @@
 const joke = document.querySelector('.joke');
 const btn = document.querySelector('button');
-const url = 'https://dad-jokes.p.rapidapi.com/random/joke';
-// console.log(joke, btn);
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1fa918ddf1mshfdcb0392a4f45b8p1dd0a5jsn566f8aca75be',
-		'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-	}
-};
+const url = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
 
-fetch(url, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+let getJoke = () => {
+    console.log(url);
+    fetch(url)
+    .then((res) => {return res.json()})
+    .then((data) => { 
+        console.log(data);
+        console.log(data.joke);
+        joke.innerHTML = data.joke;
+    })
+    .catch((error) => { console.log(error)});
+}
+
+btn.addEventListener('click', getJoke)
