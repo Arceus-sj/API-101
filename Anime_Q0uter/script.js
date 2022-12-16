b08fbca3cbe06fd99d14b1c2ec0de05ee0286534
Shantanu Jana <omni.7x@gmail.com>
2022-12-16 21:53:33 +0530

    updated script.js

--- Anime_Q0uter/script.js
+++ Anime_Q0uter/script.js
@@ -4,6 +4,34 @@ const character = document.querySelector('p .anime-character');
 const anime = document.querySelector('p .anime-name'); 
 const url = 'https://animechan.vercel.app/api/random';
 
+// custom search
+const search_btn = document.querySelector('#search-btn');
+
+search_btn.addEventListener('click', () => {
+    let user_input = document.querySelector('#search').value;
+    
+    console.log(user_input);
+
+    fetch(`${url}/anime?title=${user_input}`)
+    .then((response) => {return response.json()})
+    .then((data) => { 
+        quote.innerHTML = data.quote;
+        character.innerHTML = `— ${data.character}`;
+        anime.innerHTML = `(${data.anime})`;
+    })
+    .catch((error) => {
+        console.log(error);
+        quote.innerHTML = 'Anime not found!!!';
+        character.innerHTML = ``;
+        anime.innerHTML = ``;
+    });
+
+});
+
+
+
+
+// random quotes
 const refresh = () => {
     fetch(url)
     .then((response) => {return response.json()})
